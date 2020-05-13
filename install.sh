05bd69e2d81683cb4a20a57f0aeda0fbd650953a
Max Dymond <max.dymond@metaswitch.com>
2020-05-13 17:27:20 +0100

    Get Python 3.4 builds working again
    Signed-off-by: Max Dymond <max.dymond@metaswitch.com>

--- install.sh
+++ install.sh
@@ -2,4 +2,17 @@
 
 set -ex
 
-pip install -U poetry tox-travis tomlq python-coveralls
+# Because poetry doesn't support dev-dependency extras, we need to install these
+# tools outside of the tox poetry environment (because they're not required inside).
+#
+# python-coveralls is not specific enough about its dependencies so on Python 3.4
+# forcibly specify pyYAML 5.2, as this was the last release to support Python 3.4.
+ISNEW=$(python -c "print(1 if __import__('sys').version_info >= (3,5) else 0)")
+if (( ISNEW ))
+then
+  COVERALLS="python-coveralls"
+else
+  COVERALLS="pyYAML==5.2 python-coveralls"
+fi
+
+pip install -U poetry tox-travis tomlq $COVERALLS
