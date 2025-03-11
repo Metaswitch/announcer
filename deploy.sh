3903a7b83a2a5d88ec51558b916d978260748206
Max Dymond <max.dymond@alianza.com>
2025-03-11 16:25:13 +0000

    Fix PyPI publishing
    Signed-off-by: Max Dymond <max.dymond@alianza.com>

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
-VERSION=$(tomlq -r '.tool.poetry.version' pyproject.toml)
+VERSION=$(tomlq -r '.project.version' pyproject.toml)
 echo "Version: $VERSION"
 
 # Log into docker
 docker login -u $DOCKER_USER -p $DOCKER_PASS
-
-echo "Deploying to pypi"
-poetry publish --username $PYPI_USER --password $PYPI_PASS --build
 
 # Try and compile the Docker image - this will fail while the package isn't yet released.
 while ! docker build --build-arg VERSION=$VERSION -t metaswitch/announcer:$VERSION .
